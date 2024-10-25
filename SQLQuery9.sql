715c6edb28885e54603949c491da0a09b207b922
Lucianna Mendonca <luciannamend@gmail.com>
2024-10-24 21:08:05 -0400

    Added RDS movieplatformdb config and Mapped User model/controller

---
+++ SQLQuery9.sql
@@ -0,0 +1,12 @@
+﻿CREATE DATABASE movieplatformdb;
+GO
+
+USE movieplatformdb;
+GO
+
+CREATE TABLE [dbo].[user]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	[UserName] NVARCHAR(255) NOT NULL,
+	[Password] NVARCHAR(255) NOT NULL,
+)
---
+++ movie-platform/Models/MovieplatformdbContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace movie_platform.Models;
+
+public partial class MovieplatformdbContext : DbContext
+{
+    public MovieplatformdbContext()
+    {
+    }
+
+    public MovieplatformdbContext(DbContextOptions<MovieplatformdbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+         => optionsBuilder.UseSqlServer("");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__user__3214EC07EB2C1BAC");
+
+            entity.ToTable("user");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Password).HasMaxLength(255);
+            entity.Property(e => e.UserName).HasMaxLength(255);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
